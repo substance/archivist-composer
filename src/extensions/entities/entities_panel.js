5068716a6c332fe67b84d3d7803a273ec7e2d224
Michael Aufreiter <michael@substance.io>
2015-03-14 00:48:01 +0100

    Render entities

--- src/extensions/entities/entities_panel.js
+++ src/extensions/entities/entities_panel.js
@@ -3,6 +3,28 @@ var Component = Application.Component;
 var $$ = Application.$$;
 var _ = require("underscore");
 
+// Example of a sub view
+// ----------------
+
+var EntityView = function(props) {
+  Component.call(this, props);
+  console.log('props', props);
+};
+
+EntityView.Prototype = function() {
+
+  this.render = function() {
+    return $$("div", {className: "entity"},
+      $$("div", {className: "name", html: this.props.name}),
+      $$("div", {className: "meh", html: "meh"})
+    );
+  };
+};
+
+EntityView.Prototype.prototype = Component.prototype;
+EntityView.prototype = new EntityView.Prototype();
+
+
 // Entities Panel extension
 // ----------------
 
@@ -13,8 +35,13 @@ var EntitiesPanel = function(props) {
 EntitiesPanel.Prototype = function() {
 
   this.render = function() {
+
+    var entityNodes = this.props.map(function(entity, index) {
+    	return $$(EntityView, entity)
+    });
+
     return $$("div", {className: "entities-panel-component"},
-      $$("div", {html: "I AM THE ENTITIES PANEL"})
+      entityNodes
     );
   };
 };
