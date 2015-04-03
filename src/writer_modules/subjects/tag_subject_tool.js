11cf05a49a29413bce71b62c257e75e3d19dd119
Michael Aufreiter <michael@substance.io>
2015-04-03 20:49:07 +0200

    Update active state of subject tool.

--- src/writer_modules/subjects/tag_subject_tool.js
+++ src/writer_modules/subjects/tag_subject_tool.js
@@ -6,6 +6,36 @@ var $$ = React.createElement;
 var TagSubjectTool = React.createClass({
   displayName: "TagSubjectTool",
 
+  componentDidMount: function() {
+    var writerCtrl = this.props.writerCtrl;
+    writerCtrl.connect(this, {
+      'selection:changed': this.handleSelectionChange
+    });
+  },
+
+  handleSelectionChange: function(sel) {
+    var writerCtrl = this.props.writerCtrl;
+
+    // TODO: this will allow container selections (!)
+    if (sel.isNull() || !sel.isPropertySelection()) {
+      this.setState({
+        active: false,
+        selected: false
+      });
+    } else {
+      var range = sel.getTextRange();
+      var annotations = writerCtrl.doc.annotationIndex.get(sel.getPath(), range[0], range[1], "subject_reference");
+
+      var selected = annotations.length > 0;
+      var active = !sel.isCollapsed();
+
+      this.setState({
+        active: active,
+        selected: selected
+      });
+    }
+  },
+
   handleClick: function() {
     var writerCtrl = this.props.writerCtrl;
 
@@ -21,9 +51,20 @@ var TagSubjectTool = React.createClass({
     });
   },
 
+  getInitialState: function() {
+    return {
+      active: false,
+      selected: false
+    };
+  },
+
   render: function() {
+    var classNames = ['tag-entity-tool-component', 'tool'];
+    if (this.state.active) classNames.push("active");
+    if (this.state.selected) classNames.push("selected");
+
     return $$("a", {
-      className: 'tag-subject-tool-component tool',
+      className: classNames.join(' '),
       href: "#",
       dangerouslySetInnerHTML: {__html: '<i class="fa fa-tag"></i>'},
       title: 'Tag Subject',
