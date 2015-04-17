d0a08da6716567c8014cdd47494f5cb167b73b9b
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-04-17 15:00:32 +0200

    Comment.

--- src/interview/modules/base/text_node.js
+++ src/interview/modules/base/text_node.js
@@ -1,9 +1,7 @@
 var Article = require('substance/article');
 var Paragraph = Article.Paragraph;
 
-// TODO: remove this and use Paragraph instead.
-// Paragraph is a much more consistent and conventional
-// concept of what is desired here (text-ish block-level element).
+// Note: in archivist paragraphs are called text nodes.
 var TextNode = Paragraph.extend({
   name: "text"
 });
