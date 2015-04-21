28d18e9a8dcb204dd403bcb6eb1447ff770a3888
Michael Aufreiter <michael@substance.io>
2015-04-21 22:50:08 +0200

    Fix up title editor and remove archivist specific stuff from writer repo.

--- src/writer_modules/archivist/index.js
+++ src/writer_modules/archivist/index.js
@@ -1,8 +1,12 @@
 "use strict";
 
 var stateHandlers = require("./state_handlers");
+var ContentEditor = require("./content_editor");
 
 module.exports = {
+  components: {
+    "container": ContentEditor,
+  },
   name: "archivist",
   panels: [],
   stateHandlers: stateHandlers,
